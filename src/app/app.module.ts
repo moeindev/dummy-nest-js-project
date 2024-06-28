0158432761f5d8f3775eb8756d3dca623ad67928
Mohammad Moein Abdi <mohammad.abdi@ampada.com>
2024-06-28 21:58:30 +0330

    project-configuration: implement env loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,19 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { RepositoryModule } from './repository/repository.module';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from '../util';
 
 @Module({
-  imports: [UserModule, CategoryModule, ProductModule, RepositoryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envSchema,
+    }),
+    UserModule,
+    CategoryModule,
+    ProductModule,
+    RepositoryModule,
+  ],
 })
 export class AppModule {}
